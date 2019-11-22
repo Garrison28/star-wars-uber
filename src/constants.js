f04b977d7d62cb61b08eeff83e62f48d09d1628d
gtol <gtoledo342@gmail.com>
2019-11-22 02:55:22 -0800

    Added server urls

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const PILOTS_URL = ''
-export const PLANET_URL = ''
-export const TOUR_GUIDE_URL = ''
-export const SHIPS_URL = ''
+export const PILOTS_URL = 'http://localhost:3001/pilots'
+export const PLANET_URL = 'http://localhost:3001/planets'
+export const TOUR_GUIDE_URL = 'http://localhost:3001/guides'
+export const SHIPS_URL = 'http://localhost:3001/starships'
 
