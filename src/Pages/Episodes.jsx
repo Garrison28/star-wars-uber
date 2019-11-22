1f5df045bba5e21d3a0796eb55d25b49e5c9f29c
Garrison28 <ghighsmith28@gmail.com>
2019-11-22 10:59:10 -0800

    added img folder

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Carousel from '../Components/Carousel';
 
 const Episodes = () => {
+  const [episodes, setEpisodes] = useState([])
+  useEffect(() => {
+    axios.get('https://swapi.co/api/films/')
+      .then((response) => {
+        setEpisodes(response.data)
+      })
+  }, [])
 
+  return(
+    <Carousel items={episodes} />
+)
 }
 
 export default Episodes;
